a5322facebfda9c337173cd69453b5beda046ea2
Plucks77 <pedro_leoti@hotmail.com>
2020-05-21 11:04:59 -0300

    Substituição da url do localhost pela do heroku na configuração do axios

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3333/",
+  baseURL: "https://tcc-backend-turismo.herokuapp.com/",
 });
 //"https://tcc-backend-turismo.herokuapp.com/"
+//"http://127.0.0.1:3333/"
 export default api;
