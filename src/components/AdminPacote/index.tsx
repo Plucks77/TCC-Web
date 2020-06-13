0b336ef60ba31721f9bd46244e524343b8f8c84a
Plucks77 <pedro_leoti@hotmail.com>
2020-06-13 18:18:10 -0300

    Criação da tela de edição de um pacote e seus estilos

---
+++ src/components/AdminPacote/index.tsx
@@ -0,0 +1,322 @@
+import React, { useEffect, useState } from "react";
+
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
+
+import InputMask from "react-input-mask";
+
+import { Formik } from "formik";
+import * as yup from "yup";
+
+import { useHistory } from "react-router-dom";
+
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
+
+import { ModalDeleteConfirmation as Modal } from "../ModalDeleteConfirmation";
+
+import api from "../../api";
+import {
+  Container,
+  SpinnerContainer,
+  Header,
+  Icone,
+  FileiraCampos,
+  Campo,
+  Titulo,
+  Input,
+  InputTel,
+  Select,
+  Option,
+  FileiraDescricao,
+  InputDescricao,
+  CampoDescricao,
+  BotoesContainer,
+  Main,
+  Botao,
+  LoadingContainer,
+  Erro,
+  ModalContainer,
+} from "./styles";
+
+const pacoteSchema = yup.object({
+  name: yup
+    .string()
+    .required("O nome do pacote é necessário!")
+    .min(5, "O nome do pacote deve ter pelo menos 5 dígitos!"),
+  description: yup
+    .string()
+    .required("A descrição do pacote é necessária!")
+    .min(5, "A descrição do pacote deve ter pelo menos 5 dígitos!"),
+  price: yup.number().required("É necessário definir um preço para o pacote!"),
+});
+
+interface pacote {
+  id: number;
+  category_id: number;
+  guia_id: number;
+  name: string;
+  description: string;
+  price: string;
+}
+
+interface guia {
+  id: number;
+  name: string;
+}
+
+interface category {
+  id: number;
+  name: string;
+}
+
+interface historyPacoteId {
+  pacote_id: string;
+}
+
+const AdminPacote: React.FC = () => {
+  const [pacote, setPacote] = useState<pacote>();
+  const [guias, setGuias] = useState<guia[]>([]);
+  const [categories, setCategories] = useState<category[]>([]);
+  const [ready, setReady] = useState(false);
+  const [deleting, setDeleting] = useState(false);
+
+  const history = useHistory();
+  let config = {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const admin_id = localStorage.getItem("admin_id");
+
+    if (token && admin_id) {
+      setReady(true);
+    } else {
+      history.push("/Admin/login");
+    }
+  }, []);
+
+  useEffect(() => {
+    const history_pacote_id = history.location.state;
+    const { pacote_id } = history_pacote_id as historyPacoteId;
+    api
+      .get(`pacote/${pacote_id}`, config)
+      .then((res) => {
+        setPacote(res.data);
+      })
+      .catch((err) => {
+        if (err.response.status === 401) {
+          localStorage.clear();
+          history.push("/Admin/login");
+        }
+      });
+  }, []);
+
+  useEffect(() => {
+    api
+      .get("list/guias", config)
+      .then((res) => {
+        if (res.status === 200) {
+          setGuias(res.data);
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 401) {
+          localStorage.clear();
+          history.push("/Admin/login");
+        }
+      });
+  }, []);
+
+  useEffect(() => {
+    api
+      .get("list/category", config)
+      .then((res) => {
+        if (res.status === 200) {
+          setCategories(res.data);
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 401) {
+          localStorage.clear();
+          history.push("/Admin/login");
+        }
+      });
+  }, []);
+
+  useEffect(() => {}, []);
+
+  function handleNavigateBack() {
+    history.goBack();
+  }
+
+  function handleLogout() {
+    localStorage.clear();
+    history.push("/Admin/login");
+  }
+
+  function handleOpenDelete() {
+    setDeleting(true);
+  }
+
+  function handleCloseModal() {
+    setDeleting(false);
+  }
+
+  function handleDelete() {
+    console.log("DELETADO");
+  }
+  return ready ? (
+    <Container>
+      <Header style={deleting ? { opacity: 0.5 } : {}}>
+        <Icone onClick={() => handleNavigateBack()}>
+          <FaLongArrowAltLeft color="#36453B" size={60} />
+        </Icone>
+
+        <Icone onClick={() => handleLogout()}>
+          <FiLogOut color="#36453B" size={60} />
+        </Icone>
+      </Header>
+
+      {deleting && (
+        <ModalContainer>
+          <Modal
+            texto={`Deseja realmente excluir o guia ${pacote?.name} ?`}
+            delete={() => handleDelete()}
+            close={() => handleCloseModal()}
+          />
+        </ModalContainer>
+      )}
+
+      {pacote ? (
+        <Formik
+          initialValues={{
+            category_id: pacote.category_id,
+            guia_id: pacote.guia_id,
+            name: pacote.name,
+            description: pacote.description,
+            price: pacote.price,
+          }}
+          validationSchema={pacoteSchema}
+          onSubmit={(values, actions) => {
+            // console.log(values);
+            api
+              .put(`pacote/edit/${pacote.id}`, values, config)
+              .then((res) => {
+                if (res.status === 200) {
+                  history.goBack();
+                }
+              })
+              .catch((erro) => {
+                if (erro.response.status === 401) {
+                  localStorage.clear();
+                  history.push("/Admin/login");
+                }
+              });
+          }}
+        >
+          {(props) => (
+            <Main onSubmit={props.handleSubmit} style={deleting ? { opacity: 0.5 } : {}}>
+              <FileiraCampos>
+                <Campo style={{ marginLeft: "5em" }}>
+                  <Titulo>Nome</Titulo>
+                  <Input
+                    readOnly={deleting ? true : false}
+                    type="text"
+                    value={props.values.name}
+                    onChange={props.handleChange("name")}
+                    onBlur={props.handleBlur("name")}
+                    maxLength={50}
+                  />
+                  <Erro>{props.touched.name && props.errors.name}</Erro>
+                </Campo>
+
+                <Campo style={{ marginRight: "5em" }}>
+                  <Titulo>Guia</Titulo>
+                  <Select onChange={props.handleChange("guia_id")} value={props.values.guia_id}>
+                    {guias.map((guia) => (
+                      <Option key={guia.id} value={guia.id}>
+                        {guia.name}
+                      </Option>
+                    ))}
+                  </Select>
+                </Campo>
+              </FileiraCampos>
+
+              <FileiraCampos>
+                <Campo style={{ marginLeft: "5em" }}>
+                  <Titulo>Categoria</Titulo>
+                  <Select
+                    onChange={props.handleChange("category_id")}
+                    value={props.values.category_id}
+                  >
+                    {categories.map((category) => (
+                      <Option key={category.id} value={category.id}>
+                        {category.name}
+                      </Option>
+                    ))}
+                  </Select>
+                </Campo>
+
+                <Campo style={{ marginRight: "5em" }}>
+                  <Titulo>Valor</Titulo>
+                  <Input
+                    readOnly={deleting ? true : false}
+                    type="number"
+                    value={props.values.price}
+                    onChange={props.handleChange("price")}
+                    onBlur={props.handleBlur("price")}
+                    maxLength={50}
+                  />
+                  <Erro>{props.touched.price && props.errors.price}</Erro>
+                </Campo>
+              </FileiraCampos>
+
+              <FileiraDescricao>
+                <CampoDescricao>
+                  <Titulo>Descrição</Titulo>
+                  <InputDescricao
+                    readOnly={deleting ? true : false}
+                    value={props.values.description}
+                    onChange={props.handleChange("description")}
+                    onBlur={props.handleBlur("description")}
+                    maxLength={150}
+                  />
+                  <Erro>{props.touched.description && props.errors.description}</Erro>
+                </CampoDescricao>
+              </FileiraDescricao>
+
+              <BotoesContainer>
+                <Botao type="submit" style={{ background: "#818BE7" }}>
+                  Salvar
+                </Botao>
+                <Botao
+                  onClick={() => handleOpenDelete()}
+                  type="button"
+                  style={{ background: "#E86868" }}
+                >
+                  Excluir
+                </Botao>
+              </BotoesContainer>
+            </Main>
+          )}
+        </Formik>
+      ) : (
+        <Main>
+          <SpinnerContainer>
+            <Loader type="ThreeDots" color="#818be7" height={100} width={100} />
+          </SpinnerContainer>
+        </Main>
+      )}
+    </Container>
+  ) : (
+    <LoadingContainer>
+      <Loader type="ThreeDots" color="#818be7" height={100} width={100} />
+    </LoadingContainer>
+  );
+};
+
+export default AdminPacote;
---
+++ src/components/AdminPacote/styles.ts
@@ -0,0 +1,165 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  min-height: 100vh;
+  background: #eeeeeeee;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 2em 2em;
+`;
+
+export const Icone = styled.div`
+  cursor: pointer;
+`;
+
+export const Main = styled.form`
+  background: rgb(129, 139, 231, 0.3);
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  min-width: 700px;
+  margin: 1em 5em;
+  border-radius: 20px;
+`;
+
+export const FileiraCampos = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const FileiraDescricao = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  min-width: 800px;
+`;
+
+export const Campo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1em;
+`;
+
+export const CampoDescricao = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 3em 5em 1em 5em;
+  width: 100%;
+  min-width: 640px;
+`;
+
+export const Titulo = styled.span`
+  font-size: 30px;
+  color: #36453b;
+  font-weight: 500;
+`;
+
+export const Input = styled.input`
+  width: 15em;
+  height: 30px;
+  border: 3px solid #818be7;
+  border-radius: 10px;
+  padding-left: 10px;
+  font-size: 20px;
+  color: #36453b;
+`;
+
+export const Select = styled.select`
+  width: 15em;
+  height: 30px;
+  border: 3px solid #818be7;
+  border-radius: 10px;
+  padding-left: 10px;
+  font-size: 20px;
+  color: #36453b;
+  cursor: pointer;
+`;
+
+export const Option = styled.option`
+  width: 15em;
+  height: 30px;
+  border: 3px solid #818be7;
+  border-radius: 10px;
+  padding-left: 10px;
+  font-size: 20px;
+  color: #36453b;
+`;
+
+export const InputTel = {
+  width: "15em",
+  height: "30px",
+  border: "3px solid #818be7",
+  borderRadius: "10px",
+  paddingLeft: "10px",
+  fontSize: "20px",
+  color: "#36453b",
+};
+
+export const InputDescricao = styled.textarea`
+  box-sizing: border-box;
+  width: 100%;
+  height: 10em;
+  border: 3px solid #818be7;
+  border-radius: 10px;
+  padding-left: 10px;
+  font-size: 20px;
+  color: #36453b;
+`;
+
+export const BotoesContainer = styled.div`
+  display: flex;
+  align-self: flex-end;
+  margin-right: 5em;
+  margin-top: auto;
+  margin-bottom: 1em;
+`;
+
+export const Botao = styled.button`
+  width: 200px;
+  height: 50px;
+  border-radius: 20px;
+  border-width: 0;
+  color: #eeeeeeee;
+  font-size: 25px;
+  margin: 0 0 0 2em;
+  cursor: pointer;
+`;
+
+export const SpinnerContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoadingContainer = styled.div`
+  display: flex;
+  flex: 1;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background: white;
+`;
+
+export const Erro = styled.small`
+  color: crimson;
+  font-size: 16px;
+  align-self: center;
+  margin-top: 1px;
+`;
+
+export const ModalContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  z-index: 30;
+`;
